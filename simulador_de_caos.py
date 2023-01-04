32d9559bbf3952d20913fc77a98e3266a6c8f011
Reinaldo Silva <reinaldoblack@hotmail.com>
2023-01-04 10:43:15 -0300

    def halteres

---
+++ simulador_de_caos.py
@@ -0,0 +1,25 @@
+# importa biblioteca
+import random
+
+#cria nossa classe
+class Academia:
+    # inicia a classe com init
+    def __init__(self):
+        self.halteres = [i for i in range(10,36) if i % 2 == 0]
+        # halteres recebe - para cada iteração de i no range especifico, (sendo que eu so quero numeros pare) se o resto da divisão for igual a 0
+        sef.porta_halteres = {}
+        #criar um dicionario que irá receber os alteres
+        self.reiniciar_o_dia ()
+        # criar função que irá iniciar o dia 
+
+    def reiniciar_o_dia(self):
+        self.porta_halteres = {i: i for i in self.halteres}
+
+    def listar_alteres(self):
+
+
+
+
+
+
+self = Academia()
